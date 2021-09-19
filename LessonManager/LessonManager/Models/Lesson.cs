5db4543b2e99eda124ff499176e33e0eb6f2c5f2
antonianikolaidou <antonianikolaidou99@gmail.com>
2021-09-19 20:47:55 +0300

    Initial Commit

---
+++ LessonManager/LessonManager/Models/Lesson.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LessonManager.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Lesson> Reqiured { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ LessonManager/LessonManager/ViewModel/OverViewModel.cs
@@ -0,0 +1,17 @@
+﻿using LessonManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LessonManager.ViewModel
+{
+    public class OverViewModel
+    {
+        public Lesson Lesson {  get; set; } = null;  
+        public string UserRole {  get; set; }
+        public TeachClass TeachClass { get; set; }
+
+
+    }
+}
